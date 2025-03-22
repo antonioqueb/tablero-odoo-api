5852624b642384e631ef70bd8239ef36e92fbcf3
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-21 23:38:38 -0600

    USERNAME

--- app/odoo_connector.py
+++ app/odoo_connector.py
@@ -6,11 +6,11 @@ class OdooConnector:
         config = current_app.config
         self.url = config['ODOO_URL']
         self.db = config['ODOO_DB']
-        self.username = config['ODOO_USER']
+        self.user = config['ODOO_USER']
         self.password = config['ODOO_PASSWORD']
 
         self.common = xmlrpc.client.ServerProxy(f"{self.url}/xmlrpc/2/common")
-        self.uid = self.common.authenticate(self.db, self.username, self.password, {})
+        self.uid = self.common.authenticate(self.db, self.user, self.password, {})
         self.models = xmlrpc.client.ServerProxy(f"{self.url}/xmlrpc/2/object")
 
     def is_connected(self):
